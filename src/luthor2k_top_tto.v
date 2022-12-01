da0b8c299d9545b483b200e9ccac6bc89e16106e
Arthur <arthur.hazleden@gmail.com>
2022-11-30 22:36:52 -0800

    Update luthor2k_top_tto.v

--- src/luthor2k_top_tto.v
+++ src/luthor2k_top_tto.v
@@ -20,6 +20,6 @@ module luthor2k_top_tto
   assign io_out[1] = baudot_ready_out;  
 
   // instatiate converter  .function_pin(top_pin)
-  baudot baudot(.vee2c6c(clk_ascii), .ve9039a(clk_baudot), .vcb44a7(baudot_input), .v7c2fea(ascii_serial_output), .v0fbb9e(baudot_ready_out));
+  main main(.vee2c6c(clk_ascii), .ve9039a(clk_baudot), .vcb44a7(baudot_input), .v7c2fea(ascii_serial_output), .v0fbb9e(baudot_ready_out));
   
 endmodule
